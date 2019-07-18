f775e312cfada4c90d62bedd74058242729a276c
Xarly <cargutalo@gmail.com>
2019-07-18 13:50:03 +0200

    stable version of autologger

--- auto_logger.py
+++ auto_logger.py
@@ -1,21 +1,40 @@
 import sys
+from os import name as os_name
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from time import sleep
+from time import sleep, localtime, strftime
 from base64 import b64decode
+from pyvirtualdisplay import Display
 
 if len(sys.argv) == 1:
     print("No argument passed!!")
-	sys.exit()
+    sys.exit()
 
-driver = webdriver.Chrome()
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--profile-directory=Default')
+chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--disable-plugins-discovery")
+chrome_options.add_argument("--start-maximized")
+
+if os_name == "posix":
+    display = Display(visible=0, size=(1200, 800))
+    display.start()
+    driver = webdriver.Chrome(executable_path='/usr/lib/chromium-browser/chromedriver',
+                              chrome_options=chrome_options)
+else:
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+
+
 # driver.add_argument(“ — incognito”)
 print("Opening the web")
+print(strftime("%m/%d/%Y, %H:%M:%S", localtime()))
 driver.get('https://www.minijuegos.com/')
 
-sleep(5) 
+sleep(44)
 print("Logging stage")
-log = driver.find_element_by_id("user-widget-no-logged")	
+# driver.find_element_by_class_name('banner_accept--X').click()
+log = driver.find_element_by_id("user-widget-no-logged")
 log.click()
 
 sleep(2)
@@ -25,7 +44,9 @@ driver.find_element_by_xpath("//*[@id='login-pwd']").send_keys(b64decode(sys.arg
 driver.find_element_by_xpath("//*[@id='login-pwd']").send_keys(webdriver.common.keys.Keys.ENTER) # webdriver.common.keys.Keys.ENTER?
 
 print("Inside of profile?!!!")
-sleep(4)
+sleep(15)
 print("Ciao!!")
 
 driver.quit()
+if os_name == "posix":
+    display.stop()
